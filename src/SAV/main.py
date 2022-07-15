66df0e2d52bee24830330c6c67ea6ab7286db257
ayush@gmail.com <ayushgoel463@gmail.com>
2022-07-15 16:12:21 +0530

    added exception handling & modified code

--- src/SAV/main.py
+++ src/SAV/main.py
@@ -1,76 +1,101 @@
-from ast import Not
+import os
+import sys
 import sqlite3
 import hashlib
 import yara
 from os import walk, path
 
 
-class File_Scanner:
+class Scanner:
+    filetoscan = ""
+    dbpath = "../../scripts/hash.db"
+    directorytoscan = ""
+    rule_file = "../../test.yar"
 
-    def __init__(self, pathtodb, filetoscan, rule_file):
-        self.pathtodb = pathtodb
-        self.conn = sqlite3.connect(pathtodb)
-        self.cur = self.conn.cursor()
-        self.filetoscan = filetoscan
-        self.rule_file = rule_file
-        print("[+]Initialzing..............................")
+    def __init__(self):
+        try:
+            self.conn = sqlite3.connect(self.dbpath)
+            self.cur = self.conn.cursor()
+        except sqlite3.Error as error:
+            print("Error while connecting to sqlite", error)
 
-    def scan_hash(self):
-        def convtohash():
+        print("[+]Initializing..............................")
+
+    def convtosha256(self):
+        try:
             with open(self.filetoscan, 'rb') as f:
                 temp = f.readline()
-                hash = hashlib.sha256(temp).hexdigest()
-                f.close()
-                return hash
-        _hash = convtohash()
-        # print(_hash)
-        self.cur.execute('SELECT * FROM "check"')
-        _mal_hash = self.cur.fetchall()
-        # for i in _lentgh: _extra +=1
-        # print(_mal_hash)
-        mallist = []
-        for i in _mal_hash:
+                sha256hash = hashlib.sha256(temp).hexdigest()
+        except IOError:
+            print("File not accessible")
+        else:
+            return sha256hash
+        finally:
+            f.close()
+
+    def scan_hash(self):
+        global malwarehashes
+        if not os.path.exists(self.filetoscan):
+            print("invalid file path")
+            self.__exit__()
+        filehash = self.convtosha256()
+        try:
+            self.cur.execute('SELECT * FROM "check"')
+            malwarehashes = self.cur.fetchall()
+        except sqlite3.Error as error:
+            print("Error : ", error)
+
+        malwarelist = []
+        for i in malwarehashes:
             _id, _virushash = i
-            # print(_virushash)
-            mallist.append(_virushash)
-        # print(mallist)
-        res = [x for x in mallist if x == _hash]
-        if res !=  []:    
+            malwarelist.append(_virushash)
+        res = []
+        # res = [x for x in malwarelist if x == filehash]
+        for x in malwarelist:
+            if x == filehash:
+                res.append(os.path.abspath(self.filetoscan))
+
+        if res:
             print("[+]-----------Malware-Found-------------[+]")
             print(res)
+            print("")
+            choice = input('Do you want to delete malicious file?(Y/N) ')
+            print(choice)
+            if choice == 'Y' or 'y':
+                # os.remove(self.filetoscan)                 wierd behaviour
+                print("file removed")  # only for testing
+            else:
+                print("file not removed")  # only for testing
             print("[+]-------------------------------------[+]")
         else:
             print("[+]File free from malware")
-        # for a in mallist:
-        #     if  a == _hash:return "Malware found"
-        #     else: return "The file scanned is perfectly fine"
 
-    def scan_yara(self, Directorytoscan):
+    def scan_yara(self):
+        if not os.path.exists(self.rule_file):
+            print("invalid rule file path")
+            self.__exit__()
+        if not os.path.exists(self.directorytoscan):
+            print("invalid directory path")
+            self.__exit__()
         print("[+]-----Scanning---through----Yara-----------[+]")
         rules = yara.compile(filepath=self.rule_file)
-        for root, _, files in walk(Directorytoscan, followlinks=False):
+        for root, _, files in walk(self.directorytoscan, followlinks=False):
             for filename in files:
-                filePath = path.join(root, filename)
-                print("scanning ", filePath)
-                matches = rules.match(filePath, fast=True,)
+                filepath = path.join(root, filename)
+                print("scanning ", filepath)
+                matches = rules.match(filepath, fast=True, )
                 print(matches)
-
-
-
-
 
     def __exit__(self):
         self.conn.close()
         print("[+]Exiting...............................")
+        sys.exit()
+
 
 if __name__ == "__main__":
-    pathofdb = "../../scripts/hash.db"
-    filetoscan = "../../test/test.txt"
-    Directorytoscan = "../../test"
-    rule_file = "C:/Users/Ayan Ambesh/Documents/Github/Mal-ANALYSIS/SAV/test.yar"
-
-    myScanner = File_Scanner(
-        pathtodb=pathofdb, filetoscan=filetoscan, rule_file=rule_file)
+    myScanner = Scanner()
+    myScanner.filetoscan = "../../test/test.txt"
+    myScanner.directorytoscan = "../../test"
     myScanner.scan_hash()
-    myScanner.scan_yara(Directorytoscan=Directorytoscan)
-    File_Scanner.__exit__(myScanner)
+    myScanner.scan_yara()
+    Scanner.__exit__(myScanner)
