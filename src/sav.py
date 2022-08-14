0f2715639b7a0e367c7e465d4fa0f3e07a0db739
ayush <ayush@domain.com>
2022-08-14 16:14:10 +0530

    improved class structure

--- src/sav.py
+++ src/sav.py
@@ -6,8 +6,11 @@ from os import walk, path, remove
 import sys
 
 
-class FScanner:
+class sav:
+    
     db_path = "../Database/hash.db"
+    rule_file="../test/test.yar"
+    hash_list=""
 
     def __init__(self):
         try:
@@ -38,7 +41,7 @@ class FScanner:
             else:
                 print("file removed : {}".format(key))
 
-    def scan_hash(self, path_to_be_checked, hash_list="")->dict:
+    def scan_hash(self, path_to_be_checked)->dict:
 
         global malware_hashes
         if not path.exists(path_to_be_checked):
@@ -51,7 +54,7 @@ class FScanner:
                 current_file = path.join(root, filename)
                 files_to_be_checked[path.abspath(current_file)] = self.convert_to_sha256(path.abspath(current_file))
 
-        if not hash_list:
+        if not self.hash_list:
             try:
                 self.cur.execute('SELECT * FROM "check"')
                 malware_hashes = self.cur.fetchall()
@@ -65,27 +68,28 @@ class FScanner:
             
             result = {}
             for key in files_to_be_checked:
+                print("scanning ", path.abspath(key))
                 if files_to_be_checked[key] in malware_hashes_list:
                     result[path.abspath(key)] = files_to_be_checked[key]
             return result
 
-        elif hash_list:
+        elif self.hash_list:
             result = {}
-            malware_hashes_list = list(open(hash_list, "r").read().split("\n"))
+            malware_hashes_list = list(open(self.hash_list, "r").read().split("\n"))
             for key in files_to_be_checked:
                 if files_to_be_checked[key] in malware_hashes_list:
                     result[path.abspath(key)] = files_to_be_checked[key]
             return result            
 
-    def scan_yara(self, path_to_be_checked, rule_file="../test/test.yar")->dict:
-        if not path.exists(rule_file):
+    def scan_yara(self, path_to_be_checked)->dict:
+        if not path.exists(self.rule_file):
             print("invalid rule file path")
             self.__exit__()
         if not path.exists(path_to_be_checked):
             print("invalid directory path")
             self.__exit__()
 
-        rules = yara.compile(filepath=rule_file)
+        rules = yara.compile(filepath=self.rule_file)
         result = {}
         for root, _, files in walk(path_to_be_checked, followlinks=False):
             for filename in files:
@@ -96,32 +100,22 @@ class FScanner:
                     result[path.abspath(current_file)] = matches[0]
         return result
 
+    def qurantine_file_via_b64(self,result_file:dict={}):
+
+        for key in result_file:
+            
+            tmp = path.basename(key)
+            file_name=path.splitext(tmp)[0]
+            file_dir= path.dirname(key)
+            
+            with open(key,'rb') as vir:
+                virus = base64.b64encode(vir.read())
+            
+            with open(f'{file_dir}/quarantine_{file_name}.txt','w') as virw:
+                virw.write(str(virus))
+
+        self.file_remove(result_file)
+    
     def __exit__(self):
         self.conn.close()
         sys.exit()
-
-class Quarantine():
-    def __init__(self,result_file:dict={}):
-        self.file_path = []
-        for key in result_file:
-            self.file_path.append(key)
-
-    def qurantine_file_via_b64(self):
-        for q_path in self.file_path:
-            
-            tmp = path.basename(q_path)
-            file_name=path.splitext(tmp)[0]
-            
-            with open(q_path,'rb') as vir:
-                virus = base64.b64encode(vir.read())
-            
-            with open(f'../test/quarantine_{file_name}.txt','w') as virw:
-                virw.write(str(virus))
-
-
-if __name__ == "__main__":
-    pathsc = '../test'
-    myscanner = FScanner()
-    result_file_virus = myscanner.scan_hash(pathsc)
-    # Quarantine(result_file_virus).qurantine_file_via_b64()
-    print(result_file_virus)
